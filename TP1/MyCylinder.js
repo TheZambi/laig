0c327ac1aa4c2f9d83c5f8ab71835995184f666c
Henrique Ribeiro <up201806529@fe.up.pt>
2020-09-30 17:01:21 +0100

    Last commit was the wrong file, this is the correct one

--- TP1/MyCylinder.js
+++ TP1/MyCylinder.js
@@ -20,51 +20,83 @@ class MyCylinder extends CGFobject {
      * TODO: DEFINE TEXTURE COORDINATES
      */
     initBuffers() {
-        
+
 
         this.vertices = [];
         this.indices = [];
         this.normals = [];
         this.texCoords = [];
-        
-        
-        
-        var radiusDiff = (this.bottomRadius-this.topRadius);
+
+
+
+        var radiusDiff = (this.bottomRadius - this.topRadius);
 
 
         var ang = 2 * Math.PI / this.slices
         for (var j = 0; j <= this.stacks; j++) {
             for (var i = 0; i < this.slices; i += 1) {
                 this.vertices.push(
-                                    Math.sin(ang * i)*(this.bottomRadius-radiusDiff*(j/this.stacks)),
-                                    Math.cos(ang * i)*(this.bottomRadius-radiusDiff*(j/this.stacks)),
-                                    this.height*(j/this.stacks)
-                                   );
-                this.normals.push(Math.sin(2 * Math.PI * i / this.slices), Math.cos(2 * Math.PI * i / this.slices), radiusDiff/this.height);
-                this.texCoords.push((this.slices-i)/this.slices,(1-j)/this.stacks);
+                    Math.sin(ang * i) * (this.bottomRadius - radiusDiff * (j / this.stacks)),
+                    Math.cos(ang * i) * (this.bottomRadius - radiusDiff * (j / this.stacks)),
+                    this.height * (j / this.stacks)
+                );
+                this.normals.push(Math.sin(2 * Math.PI * i / this.slices), Math.cos(2 * Math.PI * i / this.slices), radiusDiff / this.height);
+                this.texCoords.push((this.slices - i) / this.slices, (1 - j) / this.stacks);
             }
         }
 
-        this.enableNormalViz();
-    
-        for(var stack = 0; stack < this.stacks; stack++)
-        for (var slice = 0; slice < this.slices; slice++) { //Reverse side so switching indices[2] with indices[1] so its counter clock-wise
-            if (slice == this.slices - 1) {
-              this.indices.push(stack*this.slices+slice, stack*this.slices+slice + 1, stack*this.slices+slice + 1 - this.slices);
-              this.indices.push(stack*this.slices+slice, stack*this.slices+slice + this.slices, stack*this.slices+slice + 1);
-            }
-            else {
-              this.indices.push(stack*this.slices+slice,stack*this.slices+ slice + 1 + this.slices, stack*this.slices+slice + 1);
-              this.indices.push(stack*this.slices+slice,stack*this.slices+ slice + this.slices, stack*this.slices+slice + 1 + this.slices);
-            }
-          }
 
-          this.vertices.push(0,0,0);
-          this.vertices.push(0,0,this.height);
-          for(var slice = 0; slice <= this.slices ; slice++)
-          {
-              this.indices(slice,slice+1,this.slices*(this.stacks+1));
-          }
+
+        for (var stack = 0; stack < this.stacks; stack++)
+            for (var slice = 0; slice < this.slices; slice++) { //Reverse side so switching indices[2] with indices[1] so its counter clock-wise
+                if (slice == this.slices - 1) {
+                    this.indices.push(stack * this.slices + slice, stack * this.slices + slice + 1, stack * this.slices + slice + 1 - this.slices);
+                    this.indices.push(stack * this.slices + slice, stack * this.slices + slice + this.slices, stack * this.slices + slice + 1);
+                } else {
+                    this.indices.push(stack * this.slices + slice, stack * this.slices + slice + 1 + this.slices, stack * this.slices + slice + 1);
+                    this.indices.push(stack * this.slices + slice, stack * this.slices + slice + this.slices, stack * this.slices + slice + 1 + this.slices);
+                }
+            }
+
+        for (var stack = 0; stack < this.stacks; stack++)
+            for (var slice = 0; slice < this.slices; slice++) { //Reverse side so switching indices[2] with indices[1] so its counter clock-wise
+                if (slice == this.slices - 1) {
+                    this.indices.push(stack * this.slices + slice, stack * this.slices + slice + 1, stack * this.slices + slice + 1 - this.slices);
+                    this.indices.push(stack * this.slices + slice, stack * this.slices + slice + this.slices, stack * this.slices + slice + 1);
+                } else {
+                    this.indices.push(stack * this.slices + slice, stack * this.slices + slice + 1 + this.slices, stack * this.slices + slice + 1);
+                    this.indices.push(stack * this.slices + slice, stack * this.slices + slice + this.slices, stack * this.slices + slice + 1 + this.slices);
+                }
+            }
+
+        this.vertices.push(0, 0, 0);
+        this.normals.push(0, 0, -1);
+        this.vertices.push(0, 0, this.height);
+        this.normals.push(0, 0, 1);
+        for (var slice = 0; slice < this.slices; slice++) {
+            if (slice == this.slices - 1) {
+                this.indices.push(slice, 0, this.slices * (this.stacks + 1));
+                this.indices.push((this.slices * this.stacks) + slice, this.slices * (this.stacks + 1) + 1, (this.slices * this.stacks));
+            } else {
+                this.indices.push(slice, slice + 1, this.slices * (this.stacks + 1));
+                this.indices.push((this.slices * this.stacks) + slice, this.slices * (this.stacks + 1) + 1, (this.slices * this.stacks) + slice + 1);
+
+            }
+        }
+        var ang = 2 * Math.PI / this.slices
+        for (var j = 0; j <=1; j++) {
+            for (var i = 0; i < this.slices; i += 1) {
+                this.vertices.push(
+                    Math.sin(ang * i) * (this.bottomRadius - radiusDiff * (j / this.stacks)),
+                    Math.cos(ang * i) * (this.bottomRadius - radiusDiff * (j / this.stacks)),
+                    j*this.height
+                );
+                this.normals.push(0, 0, j*2-1);
+                
+            }
+        }
+
+        // this.enableNormalViz();
 
         this.primitiveType = this.scene.gl.TRIANGLES;
         this.initGLBuffers();
