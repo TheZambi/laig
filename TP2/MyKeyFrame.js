bb528a666dc0bc744c0be71b3f5e41fc1878b76e
Henrique Ribeiro <up201806529@fe.up.pt>
2020-10-27 12:54:02 +0000

    MyKeyFrame constructor

--- TP2/MyKeyFrame.js
+++ TP2/MyKeyFrame.js
@@ -6,13 +6,36 @@ class MyKeyFrame{
      */
     constructor(scene, keyframes) {  
         super(scene);
+        for(let i = 0;i<keyframes.length;i++)
+        {
+            if(keyframes[i][0]=="instant")
+                this.instant = keyframes[i][1]
+            if(keyframes[i][0]=="translation")
+            {
+                this.translation=[];
+                this.translation.push(...keyframes[i].slice(1));
+            }
+            if(keyframes[i][0]=="scale")
+            {
+                this.scale=[];
+                this.scale.push(...keyframes[i].slice(1));
+            }
+            if(keyframes[i][0]=="rotationX")
+            {
+                this.rotationX=keyframes[i][1];
+            }
+            if(keyframes[i][0]=="rotationY")
+            {
+                this.rotationY=keyframes[i][1];
+            }
+            if(keyframes[i][0]=="rotationZ")
+            {
+                this.rotationZ=keyframes[i][1];
+            }
+        }
+        this.rotations=[this.rotationX,this.rotationY,this.rotationZ];
     }
   
-    /**
-     * @method initBuffers
-     * Initializes the animation transformations
-     */
-    initBuffers() {
-    }
+    
   }
   
